ee28e3f2e4ed246b578b8141f8ae61fa292475d5
Leon Revill <leon@revillwebdesign.com>
2015-12-19 10:26:48 +0000

    Updates

---
+++ src/rebel-request.js
@@ -0,0 +1,148 @@
+class Request {
+    constructor(baseUrl, method, params, payload) {
+        this.xhr = new XMLHttpRequest();
+        this.baseUrl = baseUrl;
+        this.path = undefined;
+        this.params = params;
+        this.payload = payload;
+        this.method = method;
+    }
+    at(path) {
+        this.path = path || "";
+        return this.execute();
+    }
+    execute() {
+        const url = Request.interpolate(this.baseUrl + this.path, this.params) + Request.buildParamString(this.params);
+        return new Promise((resolve, reject) => {
+            this.xhr.open(this.method.toUpperCase(), url);
+            this.xhr.onreadystatechange = function(event) {
+                if (event.target.readyState === 4) {
+                    var result = (event.target.responseText.length > 0) ? Request.fromJson(event.target.responseText) : null;
+                    if (event.target.status === 200) {
+                        resolve(result);
+                    } else {
+                        reject(result);
+                    }
+                }
+            };
+            if (["post", "put"].indexOf(this.method) > -1) {
+                this.xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+            }
+            const data = (this.payload !== undefined) ? Request.toJson(this.payload) : "";
+            this.xhr.send(data);
+        });
+    }
+    static buildParamString(params) {
+        let paramString = "";
+        if (params != undefined && Object.keys(params).length > 0) {
+            paramString = "?";
+            if (typeof params == "object") {
+                for (var key in params) {
+                    if (paramString != "?") {
+                        paramString += "&";
+                    }
+                    paramString += key + "=" + encodeURIComponent(params[key]);
+                }
+            } else {
+                paramString += params;
+            }
+        }
+        return paramString;
+    }
+    static interpolate(url, params) {
+        if (typeof params == "object") {
+            for (var key in params) {
+                const find = "{" + key + "}";
+                if (url.indexOf(find) > -1) {
+                    url = url.replace(find, params[key]);
+                    delete params[key];
+                }
+            }
+        }
+        return url;
+    }
+    static toJson(obj) {
+        let str = "";
+        if (typeof obj == "object") {
+            try {
+                str = JSON.stringify(obj);
+            } catch (e) {
+                console.error("Invalid JSON object provided. ");
+            }
+        }
+        return str;
+    }
+    static fromJson(str) {
+        let obj = null;
+        if (typeof str == "string") {
+            try {
+                obj = JSON.parse(str);
+            } catch (e) {
+                console.error("Invalid JSON string provided. ");
+            }
+        }
+        return obj;
+    }
+}
+
+class JpRequest {
+    static count;
+    constructor(baseUrl, params) {
+        if (isNaN(JpRequest.count++)) {
+            JpRequest.count = 0;
+        }
+        this.baseUrl = baseUrl;
+        this.path = undefined;
+        this.params = params;
+    }
+    at(path) {
+        this.path = path;
+        return this.execute();
+    }
+    execute() {
+        const timeout = 5000;
+        let url = this.baseUrl + this.path + Request.buildParamString(this.params);
+        return new Promise((resolve, reject) => {
+            let callback = '__callback' + JpRequest.count++;
+            const timeoutId = window.setTimeout(() => {
+                reject(new Error('Request timeout.'));
+            }, timeout);
+            window[callback] = response => {
+                window.clearTimeout(timeoutId);
+                resolve(Request.fromJson(response));
+            };
+            const script = document.createElement('script');
+            script.src = url + (url.indexOf('?') === -1 ? '?' : '&') + 'callback=' + callback;
+            script.onload = () => {
+                script.remove();
+            };
+            document.getElementsByTagName('head')[0].appendChild(script);
+        });
+    }
+}
+
+export class RebelRequest {
+    constructor(baseUrl, config) {
+        this.baseUrl = baseUrl || "";
+        this.format = "";
+        if (config != undefined) {
+            this.format = config.format || "";
+        }
+        return this;
+    }
+    get(params) {
+        return new Request(this.baseUrl, "get", params);
+    }
+    getJson(params) {
+        return new JpRequest(this.baseUrl, params);
+    }
+    post(payload, params) {
+        return new Request(this.baseUrl, "post", params, payload);
+    }
+    put(payload, params) {
+        return new Request(this.baseUrl, "put", params, payload);
+    }
+    delete(params) {
+        return new Request(this.baseUrl, "delete", params);
+    }
+}
---
+++ src/rebel-router.js
@@ -0,0 +1,94 @@
+import * as RebelCore from '../lib/rebel-core.js';
+
+export class RebelRouter {
+    constructor() {
+        this.paths = {};
+        return this;
+    }
+    add(path, ViewClass) {
+        let name = ViewClass.name.replace(/\W+/g, '-').replace(/([a-z\d])([A-Z0-9])/g, '$1-$2').toLowerCase();
+        document.registerElement(name, ViewClass);
+        if (Array.isArray(path)) {
+            path.forEach((item) => {
+                this.paths[item] = name;
+            });
+        } else {
+            this.paths[path] = name;
+        }
+        return this;
+    }
+    create(name) {
+        name = name.toLowerCase();
+        if (RebelCore.validElementTag(name) === true) {
+            let $constructor = document.registerElement(name, RebelView);
+            let $view = new $constructor();
+            $view.paths = this.paths;
+            return $view;
+        } else {
+            throw new Error("Invalid router name provided");
+        }
+    }
+    static getPathFromUrl() {
+        var result = window.location.href.match(/#(.*)$/);
+        if (result !== null) {
+            return result[1];
+        }
+    }
+    static getParamsFromUrl(regex, route, path) {
+        var result = RebelCore.parseQueryString(path);
+        var re = /{(\w+)}/g;
+        var results = [];
+        let match;
+        while (match = re.exec(route)) {
+            results.push(match[1]);
+        }
+        var results2 = regex.exec(path);
+        results.forEach(function(item, idx){
+            result[item] = results2[idx + 1];
+        });
+        return result;
+    }
+}
+
+class RebelView extends HTMLElement {
+    createdCallback() {
+        this.createShadowRoot();
+    }
+    attachedCallback() {
+        this.render();
+        window.onhashchange = (event) => {
+            if (event.newURL != event.oldURL) {
+                this.render();
+            }
+        };
+    }
+    current() {
+        const path = RebelRouter.getPathFromUrl();
+        for (const route in this._paths) {
+            let regexString = "^" + route.replace(/{\w+}\/?/g, "(\\w+)\/?");
+            regexString += (regexString.indexOf("\\/?") > -1) ? "" : "\\/?" + "([?=&\\w+]+)?$";
+            const regex = new RegExp(regexString);
+            if (regex.test(path)) {
+                let result = {};
+                result.templateName = this._paths[route];
+                result.route = route;
+                result.path = path;
+                result.params = RebelRouter.getParamsFromUrl(regex, route, path);
+                return result;
+            }
+        }
+        return null;
+    }
+    render() {
+        this.shadowRoot.innerHTML = "";
+        const result = this.current();
+        if (result !== null) {
+            let $template = document.createElement(result.templateName);
+            $template.setAttribute("rbl-url-params", JSON.stringify(result.params));
+            this.shadowRoot.appendChild($template);
+        }
+    }
+    set paths(value) {
+        this._paths = value;
+    }
+}
